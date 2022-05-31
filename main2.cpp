0c9c91c1131aacd8c9e0aa58c61e8209239fcabb
MohammedAladin <69739865+MohammedAladin@users.noreply.github.com>
2022-05-31 23:06:41 +0200

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -40,12 +40,12 @@ int WINAPI WinMain(HINSTANCE hThisInstance,
     /* Use default icon and mouse-pointer */
     wincl.hIcon = LoadIcon(NULL, IDI_APPLICATION);
     wincl.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-    wincl.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wincl.hCursor = LoadCursor(NULL, IDC_HAND);
     wincl.lpszMenuName = NULL;                 /* No menu */
     wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
     wincl.cbWndExtra = 0;                      /* structure or the window instance */
     /* Use Windows's default colour as the background of the window */
-    wincl.hbrBackground = (HBRUSH)COLOR_BACKGROUND;
+    wincl.hbrBackground = (HBRUSH)RGB(255,255,0);
 
     /* Register the window class, and if it fails quit the program */
     if (!RegisterClassEx(&wincl))
